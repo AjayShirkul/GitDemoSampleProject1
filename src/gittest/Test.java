c4a2de270bb599cf14751a96e02d0767aae2896c
Ajay <Ajay@DESKTOP-0HNG39N>
2021-09-05 22:51:36 +0530

    commiting the first class

---
+++ src/gittest/Test.java
@@ -0,0 +1,15 @@
+package gittest;
+
+public class Test
+{
+
+	public static void main(String[] args) {
+		System.out.println("This is sample git Hub Project");
+		System.out.println("This is sample git Hub Project");
+
+		
+	}
+	
+	
+	
+}
